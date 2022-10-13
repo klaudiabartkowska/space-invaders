4dd63168134f51ecb702713ca9d8fe260bfc7137
klaudia.bartkowska <klaudiabarti@gmailcom>
2022-10-14 00:30:57 +0100

    set bullets

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-//const body = document.body
+
 
 let spaceship = document.getElementById('spaceship')
 
 const spaceshipVelocity = 15; // it's an int 
 
 let container = document.getElementById('container')
+let spaceshipDiv = document.querySelector(".spaceshipDiv")
+container.append(spaceshipDiv)
 
 let playArea = container.getBoundingClientRect().width/2
 
@@ -16,34 +18,48 @@ let limit = container.getBoundingClientRect().width *0.2
 let started = false
 let pause = false 
 let restart = false 
-
-
-let aliens = document.getElementsByClassName('aliens')  // all the aliens
-
-
-console.log(aliens);
-
-let aliensStartPosition = playArea - (675/2) 
-
-
-// console.log(aliens.getBoundingClientRect()) 
-
-for (let i = 0; i < aliens.length; i++){
-  aliens[i]= aliensStartPosition + 'px'
-}
-
-console.log(aliensStartPosition);
-
-// let middleAlien = aliens.length/2
+let shootPressed = false 
 
 
 
+let aliens = Array.from(document.querySelectorAll('.aliens'))  // all the aliens
 
-// console.log(middleAlien)
+const enemy = document.getElementById('enemy')
+let gameOver = false
+let position = 0;
+let down = 40;  
+let direction = true; 
+let curentY = 0
+// enemy.style.transform = container.getBoundingClientRect().x + 'px'
+// console.log(aliens[1]); // use for remove index 
 
-//  console.log(container.getBoundingClientRect().width);
-//  console.log(spaceship.getBoundingClientRect().x);
-//  console.log(middleOfSpaceShip);
+function animate(){
+
+  position += 2; 
+
+    if((enemy.getBoundingClientRect().right < container.getBoundingClientRect().right) && direction){
+     enemy.style.transform = `translate(${position}px, ${curentY}px)` 
+    
+    }else{
+      if(direction === true){
+        curentY += 80
+        console.log("curentY", curentY)
+        enemy.style.transform = `translate(${position}px, ${curentY}px)` 
+        direction = false
+      } 
+      position -= 4;
+      enemy.style.transform = `translate(${position}px, ${curentY}px)` 
+      if(position < 4){
+        curentY += 80
+        enemy.style.transform = `translate(${position}px, ${curentY}px)` 
+        direction = true;
+      }
+    }
+      if(enemy.getBoundingClientRect().bottom >= spaceship.getBoundingClientRect().top ){
+        gameOver = true 
+      } 
+  }
+
 
   window.addEventListener('load', () =>{
     spaceship.style.position ='absolute'       
@@ -62,14 +78,14 @@ console.log(aliensStartPosition);
   switch (key){
       case 'ArrowLeft': 
     // console.log('test left', parseInt(spaceship.style.left))
-    if (spaceship.getBoundingClientRect().left > limit){
+    if (spaceship.getBoundingClientRect().left > container.getBoundingClientRect().left ){
       spaceship.style.left = parseInt(spaceship.style.left) - spaceshipVelocity +'px';
     } 
     break;
     case 'ArrowRight':
       // console.log('test right');
       // console.log(spaceship.getBoundingClientRect().right >container.getBoundingClientRect().width )
-      if (spaceship.getBoundingClientRect().right < container.getBoundingClientRect().width - limit){
+      if (spaceship.getBoundingClientRect().right < container.getBoundingClientRect().right){
         spaceship.style.left = parseInt(spaceship.style.left) + spaceshipVelocity +'px'
       }
       break;
@@ -77,34 +93,32 @@ console.log(aliensStartPosition);
 }
 
 
- function drawAllien(){
-
- }
-
-
 function shoot(key){
 
 if(key == " "){
-  
+
+  shootPressed = true; 
   let bullet = document.createElement("IMG");
    
   bullet.src = "./img/bullet.png"
   
   bullet.setAttribute("id","laser")
-  
-  container.appendChild(bullet);
+  const body= document.querySelector("body")
+  // container.appendChild(bullet); 
+  body.append(bullet)
 
-  const speed = 5;
-  const delay = 7;
   const damage = 1;
-  const bulletY = spaceship.getBoundingClientRect().y;
-  const bulletX = spaceship.getBoundingClientRect().x + middleOfSpaceShip
+  const bulletY = spaceship.getBoundingClientRect().top-13 + "px"
+  const bulletX =  ((spaceship.getBoundingClientRect().right + spaceship.getBoundingClientRect().left)/ 2)-13+'px'
+ 
 
-
- bullet.style.left = (bulletX) + 'px'
+  console.log(spaceship.getBoundingClientRect().y)
+  console.log(spaceship.getBoundingClientRect().x, "x numb")
+  bullet.style.top= bulletY 
+  bullet.style.left = bulletX
 
   console.log('bullet',bullet.getBoundingClientRect()) 
-  console.log('spaceship:',spaceship.getBoundingClientRect())
+  console.log('spaceship:',spaceship.getBoundingClientRect()) 
 
 
 }
@@ -113,10 +127,12 @@ if(key == " "){
 
   document.addEventListener('keydown', e =>{
     switch(e.key){
-      case 's': 
-        started = true; 
+      case 'Enter': 
+        started = true;
+        console.log(started); 
       case 'p': 
         pause = true;
+        console.log(pause);
       case 'r':
         restart = true;
       case " ":
@@ -132,11 +148,16 @@ if(key == " "){
 
   
     function gameLoop(){
-    drawAllien()
+    if(started === true){
+      if(gameOver === false) {
+      animate();
+    }
     shoot();
 
+  }
     requestAnimationFrame(gameLoop)
   }
+
     
    window.requestAnimationFrame(gameLoop)
     
